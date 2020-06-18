5b2096922ed7748f64526be383c5d61b4d983334
Ethan Printz <ethanprintz@gmail.com>
2020-06-18 15:33:42 -0400

    First layout of upper fold

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,28 @@
     <Navbar />
     <!-- Main content -->
     <v-content id="content">
-      <v-col cols="12" sm="6" md="3">
-        <!-- Issue Search Field -->
-        <v-text-field
-          label="Search Issues"
-          id="search-bar"
-          filled
-          rounded
-          @click:append="issueSearch"
-          append-icon="mdi-comment-search-outline"
-        ></v-text-field>
-      </v-col>
+      <div id="upper-fold">
+        <v-col cols="12" sm="8" md="6">
+          <div id="upper-fold-button-container">
+            <v-btn depressed class="upper-fold-button">
+              <v-icon left dark>mdi-plus-circle-outline</v-icon>
+              <span>Post New Issue</span>
+            </v-btn>
+            <v-btn depressed class="upper-fold-button">
+              <v-icon left dark>mdi-calendar-plus</v-icon>
+              <span>Schedule Help</span>
+            </v-btn>
+          </div>
+          <!-- Issue Search Field -->
+          <v-text-field label="Search Issues" id="search-bar" filled rounded color="#787878">
+            <template v-slot:append>
+              <v-btn right icon @click="issueSearch">
+                <v-icon>mdi-comment-search-outline</v-icon>
+              </v-btn>
+            </template>
+          </v-text-field>
+        </v-col>
+      </div>
     </v-content>
   </v-app>
 </template>
@@ -38,8 +49,32 @@ export default {
 <style lang="sass">
 #content
   background-color: #E5E5E5
+  margin-top: -0.3rem
+
+#upper-fold
+  background-color: #E1E0E0
+  border-bottom: 0.3rem solid #DADADA
+  min-height: 40vh
+  display: flex
+  align-items: center
+  justify-content: flex-start
+  padding: 2rem
+  #upper-fold-button-container
+    padding: 1rem
+    .upper-fold-button
+      font-weight: 700
+      margin: 0 1rem 0 0
+      background-color: #E7E7E7
+      color: #787878
+
 #search-bar
-  color: #787878
+  color: #393939
   border-bottom-color: rgba(0,0,0,0)
   border-bottom-width: 0
+  font-weight: 800
+.v-input__slot
+  background-color: #E7E7E7!important
+.v-input__append-inner
+  margin-top: 0!important
+  align-self: center!important
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,6 +3,13 @@
     <v-app-bar flat id="app-bar">
       <h1 id="title">IMA/ITP HELPER</h1>
       <v-spacer></v-spacer>
+      <div id="name-column">
+        <div id="name">Ethan Printz</div>
+        <div id="points">
+          <v-icon small color="grey">mdi-trophy-variant-outline</v-icon>
+          <span>63 Points</span>
+        </div>
+      </div>
       <v-avatar>
         <!-- Profile Image - Change Later -->
         <img
@@ -25,7 +32,7 @@ export default {};
   background-color: #A9A9A9
   font-weight: 900
   padding: 0 6px
-  letter-spacing: 0.1rem
+  letter-spacing: 0.15rem
   font-size: 1.6rem
 
 #app-bar
@@ -34,4 +41,28 @@ export default {};
 
 #profileImage
   border: 4px solid #C2C2C2
+
+#name-column
+  height: 100%
+  min-width: 8vw
+  display: flex
+  flex-direction: column
+  align-items: flex-end
+  justify-content: center
+  margin-right: 1rem
+  color: grey
+
+#name
+  font-size: 1.1rem
+  font-weight: 800
+  letter-spacing: 0.03rem
+
+#points
+  font-size: 0.85rem
+  display: flex
+  align-items: center
+  justify-content: flex-end
+  font-weight: 700
+  color: #9E9E9E
+  margin-top: -0.5vh
 </style>
